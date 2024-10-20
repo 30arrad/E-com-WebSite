854b5748701a0ec3508579efc7b644325265f267
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-10-20 22:50:36 +0600

    Work Men Page
    add Men Pants Items and Men T-Shirt Items

---
+++ Men/DataBase/t_shirt.js
@@ -0,0 +1,62 @@
+const addT_ShirtItems = [
+    {
+    image: '../Men/image/t-shirt/1.png',
+    name: 'Arrad Ahmed',
+    company: 'Arrad Ahmed is a Good Boy. nsfdkjhg kgfhsdkgf sdfgsd fglisdfuhj sdfgsgsd ',
+    newprice: 3000,
+    oldprice: 100000,
+    dicount: 70,
+    rating: {
+        rate: 4.5,
+        count: 1400
+    }
+},
+{
+    image: '../Men/image/t-shirt/2.png',
+    name: ' Ahmed',
+    company: 'He is a Good Boy.',
+    newprice: 3000,
+    oldprice: 100000,
+    dicount: 70,
+    rating: {
+        rate: 4.5,
+        count: 1400
+    }
+},
+{
+    image: '../Men/image/t-shirt/3.png',
+    name: 'ahkhas Ahmed',
+    company: 'Asheis a Good girl\.',
+    newprice: 3000,
+    oldprice: 100000,
+    dicount: 70,
+    rating: {
+        rate: 4.5,
+        count: 1400
+    }
+},
+{
+    image: '../Men/image/t-shirt/4.png',
+    name: 'Arrad dfs',
+    company: 'Arrad Ahmesdgsfdgh gdgedy.',
+    newprice: 3000,
+    oldprice: 100000,
+    dicount: 70,
+    rating: {
+        rate: 4.5,
+        count: 1400
+    }
+},
+{
+    image: '../Men/image/t-shirt/5.png',
+    name: 'Arradgrytuyu Ahmed',
+    company: 'Arrad Ahmed iood Boy.',
+    newprice: 3000,
+    oldprice: 100000,
+    dicount: 70,
+    rating: {
+        rate: 4.5,
+        count: 1400
+    }
+},
+];
--- Men/men.js
+++ Men/men.js
@@ -1,5 +1,12 @@
 
-const Add = document.getElementById('item-div');
+Pants();
+T_Shirt();
+
+
+// --------------------------Pants------------------------------------
+
+function Pants() {
+    const Add = document.getElementById('item-div');
 // console.log(Add.innerHTML); 
 
 let innerHTML = '';
@@ -28,3 +35,38 @@ addItems.forEach(addItems => {
 Add.innerHTML = innerHTML;
 
 // console.log(Add.innerHTML);
+}
+
+// --------------------------T-Shirt------------------------------------
+
+function T_Shirt() {
+    const T_Shirt = document.getElementById('T_Shirt');
+// console.log(Add.innerHTML); 
+
+let innerHTML = '';
+addT_ShirtItems.forEach(addT_ShirtItems => {
+    
+    innerHTML += `<div class="category_items">
+            <div>
+                <a href="#"><img class="sale_item" src="${addT_ShirtItems.image}"></a>
+            </div>
+            <div class="rating">
+                ${addT_ShirtItems.rating.rate} ⭐ | ${addT_ShirtItems.rating.count}
+            </div>
+            <div class="company-name">${addT_ShirtItems.name}</div>
+            <div class="item-name">${addT_ShirtItems.company}</div>
+            <div class="price">
+                <span class="current-price">Tk ${addT_ShirtItems.newprice}</span>
+                <span class="original-price">Tk ${addT_ShirtItems.oldprice}</span>
+                <span class="discount">(${addT_ShirtItems.dicount}% Loss)</span>
+            </div>
+            <div class="Button_add">
+                <button class ="buy_button">Add to Cart</button>
+                <button class ="Wishlist_button">Wishlist</button>
+            </div>
+        </div>`
+});
+T_Shirt.innerHTML = innerHTML;
+
+// console.log(Add.innerHTML);
+}
