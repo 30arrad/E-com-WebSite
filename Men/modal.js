4396074e6116b7d436024dea5e08dae30093d38f
Arrad Ahmed <110816803+30arrad@users.noreply.github.com>
2024-10-23 20:54:02 +0600

    add sum code

--- Men/modal.js
+++ Men/modal.js
@@ -1,3 +1,75 @@
+PantsModel();
+
+function PantsModel() {
+    const Add = document.getElementById('sizeModal');
+// console.log(Add.innerHTML); 
+
+let innerHTML = '';
+addItems.forEach(addItems => {
+    
+    innerHTML += `<div class="modal-content">
+            <span class="close">&times;</span>
+            <h2>Price Details</h2>
+            <hr>
+            <div class ="divModalBox">
+                <div class ="modalLeft">
+                    <br>
+                    <p id="modalLeft_L">${addItems.name}</p>
+                    <br>
+                    <img class ="modal_img" src="${addItems.image}" alt="image">
+               </div>
+               <div class="size-options">
+                
+                <p id="modalLeft_R">Choose Size:</p>
+               
+                   <button class="size-btn">M</button>
+                   <button class="size-btn">L</button>
+                   <button class="size-btn">XL</button>
+                   <button class="size-btn">XXL</button>
+
+                <div class="price">
+                <span class="new-price">৳ ${addItems.newprice}</span>
+                <span class="old-price">৳ ${addItems.oldprice}</span>
+                <span class="discount">(${addItems.dicount}% Loss)</span>
+                </div>
+               </div>
+            </div>
+            
+            <button class="modal-cancel-btn">Cancel</button>
+            <button class="modal-add-cart-btn"onclick="addToCart(${addItems.id})">Add to Cart</button>
+        </div>`
+});
+Add.innerHTML = innerHTML;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Get modal element
 const modal = document.getElementById('sizeModal');
 
@@ -27,7 +99,6 @@ document.querySelector('.modal-cancel-btn').addEventListener('click', () => {
 // Add functionality to 'Add to Cart' button (currently just closes modal)
 document.querySelector('.modal-add-cart-btn').addEventListener('click', () => {
     modal.style.display = 'none';
-
     alert('Item added to cart');
 });
 
